75e340063ce155b59a17d875d568b579515c9496
Tono <tonoyama0229@gmail.com>
2022-05-22 22:21:16 +0900

    first

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+
+
+export default function Header() {
+
+
+  return (
+    <header>
+      <div className="header-left">
+        <h1>Subsc Square</h1>
+      </div>
+
+      <div className="header-right">
+        <div className="nav-item">
+          <p>投稿一覧</p>
+        </div>
+
+        <div className="nav-item">
+          <p>投稿する</p>
+        </div>
+
+
+      </div>
+    </header>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div className='home-text'>
+        <h2>Subsc Squareは～</h2>
+  
+        <p>説明文</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
